ee7fa086299623de296e18551e6b44f2192b8821
Christian Krenslehner <christian.krenslehner@sloc.one>
2025-03-12 11:42:30 +0100

    stm32wb55 example

--- minimal-firmware/build.rs
+++ minimal-firmware/build.rs
@@ -1,9 +1,15 @@
+use std::env;
+
 fn main() {
-    // apply to all targets
+    // stm32 specific
     println!("cargo:rustc-link-arg=--nmagic");
     println!("cargo:rustc-link-arg=-Tlink.x");
-    println!("cargo:rustc-link-arg=-Tdefmt.x");
 
-    // only applies to tests
-    println!("cargo:rustc-link-arg-tests=-Tembedded-test.x");
+    // add linker script for embedded-test!!
+    println!("cargo::rustc-link-arg-tests=-Tembedded-test.x");
+
+    // Check if the `defmt` feature is enabled, and if so link its linker script
+    if env::var("CARGO_FEATURE_DEFMT").is_ok() {
+        println!("cargo:rustc-link-arg=-Tdefmt.x");
+    }
 }
--- minimal-firmware/tests/simple_test.rs
+++ minimal-firmware/tests/simple_test.rs
@@ -1,15 +1,61 @@
 #![no_std]
 #![no_main]
 
-#[cfg(test)]
-#[embedded_test::tests]
-mod tests {
-    use defmt_rtt as _;
-    use embassy_stm32 as _;
 
+fn setup_log() {
+    #[cfg(feature = "log")]
+    rtt_target::rtt_init_log!();
+    #[cfg(feature = "defmt")]
+    rtt_target::rtt_init_defmt!();
+}
+
+#[cfg(test)]
+/// Need to explicitly state the executer
+#[embedded_test::tests(executor = embassy_executor::Executor::new(), setup=crate::setup_log())]
+mod tests {
+    use rtt_target as _;
+    use embassy_stm32::Peripherals;
+
+    // An optional init function which is called before every test
+    // Asyncness is optional, so is the return value
+    #[init]
+    async fn init() -> Peripherals {
+        let config = embassy_stm32::Config::default();
+        let p = embassy_stm32::init(config);
+        p
+    }
+
+    // Tests can be async (needs feature `embassy`)
+    // Tests can take the state returned by the init function (optional)
     #[test]
-    fn is_ok() {
-        let _config = embassy_stm32::Config::default();
-        assert!(true);
+    async fn takes_state(_state: Peripherals) {
+        assert!(true)
+    }
+
+    // Tests can be ignored with the #[ignore] attribute
+    #[test]
+    #[ignore]
+    fn it_works_ignored() {
+        assert!(false)
+    }
+
+    // Tests can fail with a custom error message by returning a Result
+    #[test]
+    fn it_fails_with_err() -> Result<(), &'static str> {
+        Err("It failed because ...")
+    }
+
+    // Tests can be annotated with #[should_panic] if they are expected to panic
+    #[test]
+    #[should_panic]
+    fn it_passes() {
+        assert!(false)
+    }
+
+    // Tests can be annotated with #[timeout(<secs>)] to change the default timeout of 60s
+    #[test]
+    #[timeout(10)]
+    fn it_timeouts() {
+        loop {} // should run into the 10s timeout
     }
 }
